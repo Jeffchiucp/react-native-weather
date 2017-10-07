4be40df295e205e888cccb99302bcba244192fb8
Jeffchiucp <jeffchiu2000@gmail.com>
2017-10-07 09:12:22 -0700

    added DarkSky API

--- App.js
+++ App.js
@@ -1,9 +1,58 @@
-
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import DayTemp from './dayTemp';
 
 export default class App extends React.Component {
+    constructor(props){
+        super(props);
+
+        // this.days = [];
+
+        this.state = {
+            weatherDescription: null,
+            humidity: null,
+            wind: null
+        }
+    }
+
+    componentWillMount(){
+        //set the state of the weather description
+        // this.findWeather();
+    }
+
+    findWeather() {
+        const apikey = '71043c8b60e60a460020f46b78c6850c';
+        const path = `https://api.darksky.net/forecast/71043c8b60e60a460020f46b78c6850c/37.8267,-122.4233`;
+        fetch(path)
+        .then(res => res.json())
+        .then((json) => {
+            // console.log(json);
+            // if (json.cod === 200){
+                const description = json.weather[0].main;
+                const weatherHumidity = json.main.humidity;
+                const windValue = json.wind.speed;
+                this.setState({weatherDescription: description, humidity: weatherHumidity, wind: windValue});
+        })
+        .catch(err => console.log(err));
+    }
+    //display the weather description if it is not null
+    showWeather(){
+        if (this.state.weatherDescription){
+            return (
+                [
+                <Text key = "description" style= {styles.weatherDescription}>{this.state.weatherDescription}</Text>,
+                <Text key = "humidity" style= {styles.weatherDescription}>Humidity: {this.state.humidity}%</Text>,
+                <Text key = "wind" style= {styles.weatherDescription}>Wind: {this.state.wind}</Text>
+                ]
+            )
+
+        }else{
+            return <Text style= {styles.weatherDescription}>{''}</Text>
+        }
+    }
+
+
+
     render() {
         //Update Day
         const days = ["Sun","Mon","Tues","Wed","Thurs","Fri","Sat"];
@@ -13,26 +62,18 @@ export default class App extends React.Component {
 
         //Update Month
         var months = new Array();
-        months[0] = "January";
-        months[1] = "February";
-        months[2] = "March";
-        months[3] = "April";
-        months[4] = "May";
-        months[5] = "June";
-        months[6] = "July";
-        months[7] = "August";
-        months[8] = "September";
-        months[9] = "October";
-        months[10] = "November";
-        months[11] = "December";
+        var months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December" ]
         const monthNumber = date.getMonth();
         const month = months[monthNumber];
 
+        // utc -> new Date(utc * 1000)
+
         //Update Month
-        const weatherArray = ["77˚", "75˚", "74˚", "74˚", "70˚", "79˚", "73˚"];
+        const weatherArray = ["70˚", "70˚", "64˚", "72˚", "72˚", "69˚", "63˚"];
         const currentWeather = weatherArray[dayNumber];
-        //function we implement in the class
-        const listDays = () => {
+
+        //get all of the days/temperatures components in an array
+        const listDaysTemp = () => {
             return days.map((day, index) => {
                 return <DayTemp key = {index} valueDay = {days[index]} valueWeather = {weatherArray[index]} />
             })
@@ -44,9 +85,15 @@ export default class App extends React.Component {
                 <Text style= {styles.date}>{day}, {month} {dayNumber + 1}</Text>
                 <Text style= {styles.currentWeather}>{currentWeather}</Text>
 
-                <View style= {styles.dayContainer}>
-                    {listDays()}
+                <View style= {styles.weatherContainer}>
+                    {this.showWeather()}
                 </View>
+
+                {/* REACT COMMENT */}
+                <View style= {styles.dayContainer}>
+                    {listDaysTemp()}
+                </View>
+
             </View>
         );
     }
@@ -54,33 +101,47 @@ export default class App extends React.Component {
 
 const styles = StyleSheet.create({
     container: {
-      display: 'flex',
-      flexDirection: 'column',
-      alignItems: 'center',
-      justifyContent: 'space-around',
-      height: '100%',
-      backgroundColor: '#D3D3D3'
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+        justifyContent: 'center',
+        height: '100%',
+        backgroundColor: 'gray'
+
     },
     state: {
         fontSize: 40,
         fontWeight: '300',
-        paddingBottom: 5
+        paddingBottom: 5,
+        // flex: 1
     },
     date:{
-        fontSize: 30,
+        fontSize: 25,
         fontWeight: '200',
-        paddingBottom: 10
+        paddingBottom: 10,
+        // flex: 1
     },
     dayTemp: {
         width: 50,
         height: 50
     },
     dayContainer: {
-      flexDirection: 'row',
+       flexDirection: 'row',
     },
     currentWeather: {
-      fontSize: 60,
-      paddingBottom: 5
-    }
-
+       fontSize: 90,
+       paddingBottom: 5,
+    //    flex: 1,
+    //    backgroundColor: 'cyan'
+    },
+    weatherDescription: {
+       fontSize: 18,
+       paddingBottom: 10,
+       fontWeight: '100',
+       marginRight: 25
+   },
+   weatherContainer: {
+       flexDirection: 'column',
+       alignItems: 'center',
+   }
 });
